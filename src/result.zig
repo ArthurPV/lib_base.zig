3a7a26429b1b10d889df072a9d9bc7f499f19b76
ArthurPV <arthurpontiervalois@gmail.com>
2023-03-22 10:39:32 -0400

    Add `unwrapMut` and `unwrapMutErr` function.

--- src/result.zig
+++ src/result.zig
@@ -30,6 +30,20 @@ pub fn Result(comptime T: type, comptime E: type) type {
                 .err => |v| v,
             };
         } 
+
+        pub fn unwrapMut(self: *Self) *T {
+            return switch (self.*) {
+                .ok => |v| &v,
+                .err => |_| @panic("Failed to get ok value"),
+            };
+        }
+
+        pub fn unwrapMutErr(self: *Self) *E {
+            return switch (self.*) {
+                .ok => |_| @panic("Failed to get err value"),
+                .err => |v| &v,
+            };
+        }
     };
 }
 
