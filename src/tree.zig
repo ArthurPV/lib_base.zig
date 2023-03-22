f1fd48e1c85dcd3bb6b9eca18dfed40727f22f96
ArthurPV <arthurpontiervalois@gmail.com>
2023-03-22 11:57:46 -0400

    Update `tree.zig`.

---
+++ src/tree.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+/// General purpose Tree struct.
+pub fn Tree(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        ///            value
+        ///              ^
+        ///             / \
+        ///            /   \
+        ///           /     \
+        ///         left   right
+        ///        value   value
+        ///          ^       ^
+        ///         / \     / \
+        ///        /   \   /   \
+
+        left: ?*Tree(T) = null,
+        right: ?*Tree(T) = null,
+        value: T,
+    };
+}
