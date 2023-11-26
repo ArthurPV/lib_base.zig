c96554811b696a5306cfefffed18073a6dbb4620
ArthurPV <arthurpontiervalois@gmail.com>
2023-11-26 13:52:44 -0500

    refactor(global): Update `build.zig`.

--- build.zig
+++ build.zig
@@ -11,38 +11,22 @@ fn getCSource(path: []const u8, args: []const []const u8) std.build.CSourceFile 
 pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const mode = b.standardOptimizeOption(std.build.StandardOptimizeOptionOptions{});
+    const lib = b.addStaticLibrary(.{ .name = "base", .target = .{}, .optimize = mode, .root_source_file = .{ .path = "src/main.zig" } });
+    var base_pkg = b.addModule("base", .{ .source_file = .{ .path = "src/base.zig" } });
+    var collections_pkg = b.addModule("collections", .{ .source_file = .{ .path = "src/collections.zig" } });
 
-    const lib = b.addStaticLibrary("base", "src/main.zig");
+    lib.addModule("base", base_pkg);
+    lib.addModule("collections", collections_pkg);
 
-    const base_pkg = std.build.Pkg{
-        .name = "base",
-        .source = getFileSource("src/base.zig")
-    };
+    const main_tests = b.addTest(.{ .root_source_file = .{ .path = "src/main.zig" }, .main_pkg_path = .{ .path = "src/main.zig" } });
+    main_tests.addModule("collection", collections_pkg);
+    main_tests.addModule("base", base_pkg);
 
-    const collections_pkg = std.build.Pkg{
-        .name = "collections",
-        .source = getFileSource("src/collections.zig")
-    };
-
-    lib.addPackage(base_pkg);
-    lib.addPackage(collections_pkg);
-
-    lib.setBuildMode(mode);
-    lib.install();
-
-    const main_tests = b.addTest("src/main.zig");
-    main_tests.setBuildMode(mode);
-    main_tests.addPackage(collections_pkg);
-    main_tests.addPackage(base_pkg);
-
-    const base_tests = b.addTest("src/base.zig");
-    base_tests.setBuildMode(mode);
-
-    const collections_tests = b.addTest("src/collections.zig");
-    collections_tests.setBuildMode(mode);
-
+    const base_tests = b.addTest(.{ .root_source_file = .{ .path = "src/base.zig" }, .main_pkg_path = .{ .path = "src/main.zig" } });
+    const collections_tests = b.addTest(.{ .root_source_file = .{ .path = "src/collections.zig" }, .main_pkg_path = .{ .path = "src/main.zig" } });
     const test_step = b.step("test", "Run library tests");
+
     test_step.dependOn(&main_tests.step);
     test_step.dependOn(&base_tests.step);
     test_step.dependOn(&collections_tests.step);
